70f4a79fe32265580847b37089768cec469b7ebe
Tomas Divis <tomas.divis@nic.cz>
2012-06-19 13:56:21 +0200

    Ticket #6965 - Added automaded versioning in setup.py from Git tags/revisions

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 
 from freddist.core import setup
 
-PACKAGE_VERSION = '1.0'
 PROJECT_NAME = 'fred-pylogger'
 PACKAGE_NAME = 'fred-pylogger'
 
@@ -15,7 +14,6 @@ def main():
         author = 'Tomáš Diviš, CZ.NIC',
         author_email = 'vlastimil.zima@nic.cz',
         url = 'http://www.nic.cz/',
-        version = PACKAGE_VERSION,
         license = 'GNU GPL',
         platforms = ['posix'],
         packages = ['pylogger']
