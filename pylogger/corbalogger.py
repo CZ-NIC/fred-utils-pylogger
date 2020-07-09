24d12572f43b1e58d5c1c8ba6d74d9eb01c1f6a5
Vlastimil Zíma <vlastimil.zima@nic.cz>
2020-07-09 09:55:04 +0200

    Ticket #28088 - Fix isort

--- pylogger/corbalogger.py
+++ pylogger/corbalogger.py
@@ -17,7 +17,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with FRED.  If not, see <https://www.gnu.org/licenses/>.
-
 """Logging framework."""
 from __future__ import unicode_literals
 
